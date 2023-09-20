d6065e010a7f5b3ce625eb7bfdc167963abf26ed
rrodrigodlima <rdrigo019@gmail.com>
2023-09-20 02:21:29 -0300

    created a struct for colors

--- src/RentCars/Types/Structs/Color.cs
+++ src/RentCars/Types/Structs/Color.cs
@@ -3,4 +3,12 @@ namespace RentCars.Types;
 //1 - Crie uma `Struct` para as **Cores**
 public struct Color
 {
+  public string Name { get; set; }
+  public string Hex { get; set; }
+
+  public Color(string name, string hex)
+  {
+    Name = name;
+    Hex = hex;
+  }
 }
