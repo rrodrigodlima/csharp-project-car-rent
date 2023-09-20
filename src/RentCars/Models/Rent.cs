4d83a2c9c2b48fb3e532724f9308563fe2d9a082
rrodrigodlima <rdrigo019@gmail.com>
2023-09-20 11:08:52 -0300

    created the rent feature

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -13,7 +13,27 @@ public class Rent
     //10 - Crie o construtor de `Rent` seguindo as regras de negócio
     public Rent(Vehicle vehicle, Person person, int daysRented)
     {
-        throw new NotImplementedException();
+        Vehicle = vehicle;
+        Person = person;
+        DaysRented = daysRented;
+        Status = RentStatus.Confirmed;
+
+        // Verificar se o locatário é uma pessoa jurídica
+        if (person is LegalPerson)
+        {
+            // Pessoa jurídica recebe 10% de desconto
+            Price = vehicle.PricePerDay * daysRented * 0.9;
+        }
+        else
+        {
+            Price = vehicle.PricePerDay * daysRented;
+        }
+
+        // Alterar o atributo IsRented do veículo para true
+        vehicle.IsRented = true;
+
+        // Alterar o débito da pessoa para o preço calculado
+        person.Debit += Price;
     }
 
     //11 - Implemente os métodos de `cancelar` e `finalizar` um aluguel
