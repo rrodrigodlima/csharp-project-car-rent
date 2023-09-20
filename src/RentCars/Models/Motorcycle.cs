b80519e8d893a2e3b3096a4090e2168c2009aebb
rrodrigodlima <rdrigo019@gmail.com>
2023-09-20 11:15:44 -0300

    changes

--- src/RentCars/Models/Motorcycle.cs
+++ src/RentCars/Models/Motorcycle.cs
@@ -6,6 +6,6 @@ namespace RentCars.Models;
 public class Motorcycle : Vehicle
 {
   public double SeatHeight { get; set; }
-  public BrakeType FrontBrakeType { get; set; }
-  public BrakeType RearBrakeType { get; set; }
+  public BrakeType FrontBrake { get; set; }
+  public BrakeType RearBrake { get; set; }
 }
